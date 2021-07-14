f7ecb48ec4b38b005b1bd78e3ab851210fabe6ab
DKS007 <darpankumarsingworld42@gmail.com>
2021-07-14 11:13:07 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int largest(int arr[], int n);
+
+int main()
+{
+
+    int arr[1000];
+    int i,t,n;
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d", &n);
+        for(i=0;i<n;i++)
+            scanf("%d", &arr[i]);
+        printf("%d", largest(arr, n));
+        printf("\n");
+    }
+    return 0;
+}
+
+
+int largest(int arr[], int n)
+{
+    int max,j;
+    max=arr[0];
+    for(j=0;j<n;j++)
+    {
+        if(max>arr[j])
+            max=max;
+        else
+            max=arr[j];
+    }
+    return max;
+}
